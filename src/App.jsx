465dbaea2aca2db33720e3109871a224fddec8f9
assilbekov <askhat.assilbekov@gmail.com>
2024-04-07 08:25:04 -0300

    init background

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="w-full relative">
+      APp page
+    </main>
+  )
 }
 
 export default App;
